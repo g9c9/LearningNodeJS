d7b79ae7c5b2ee2a4478891a074d173e2f1bbb9e
George <g9c9@outlook.com>
2021-09-09 11:44:17 -0700

    Standard output

---
+++ questions.js
@@ -0,0 +1,19 @@
+// These objects are used for input and outpur
+// process.stdin
+// process.stdout
+
+//process.stdout.write("Hello ");
+//process.stdout.write("World \n\n\n");
+
+const questions = [
+  "What is your name?",
+  "What would you rather be doing?",
+  "What is your preferred programming language?",
+];
+
+const ask = (i = 0) => {
+  process.stdout.write(`\n\n\n ${questions[i]}`);
+  process.stdout.write(` > `);
+};
+
+ask();
