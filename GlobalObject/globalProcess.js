ba283f8305085e49b8a69564d780856d75eb00e8
George <g9c9@outlook.com>
2021-09-09 11:15:56 -0700

    accessing process and its properties

---
+++ GlobalObject/globalProcess.js
@@ -0,0 +1,5 @@
+// process object can be accessed globally
+// contains information about the current process and tools
+// to interact with process
+console.log(process.pid);
+console.log(process.versions.node);
