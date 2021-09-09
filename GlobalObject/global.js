20f704ea53240eec3ead718770f3aaeceebad630
George <g9c9@outlook.com>
2021-09-09 10:57:31 -0700

    Added comments

--- GlobalObject/global.js
+++ GlobalObject/global.js
@@ -1,6 +1,6 @@
 const path = require("path");
 
-//console.log(__dirname);
-//console.log(__filename);
+//console.log(__dirname);   // __dirname is global
+//console.log(__filename);  // __filename is global
 
 console.log(`The file name is ${path.basename(__filename)}`);
